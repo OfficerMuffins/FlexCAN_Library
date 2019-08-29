8eaf6a2a8346d01c0fecadfd4594bddfd925c0d8
khang <iloveuzura@gmail.com>
2019-08-29 18:47:00 -0400

    initial commit

---
+++ receiver/receiver.ino
@@ -0,0 +1,77 @@
+#include "FlexCAN.h"
+#include "kinetis_flexcan.h"
+#include <SD.h>
+#include <SPI.h>
+
+#define CAN_TEST 1
+
+// TODO: take message from CAN frame and write to SD card
+// hello world
+#if CAN_TEST == 0
+#else
+int led = 13;
+// create CAN object
+FlexCAN CANReceiver(16000000/100000);
+static CAN_message_t msg;
+static CAN_filter_t filter;
+
+typedef struct uint128_t { unsigned char x[16]; } uint128_t;
+#endif
+
+File myFile;
+
+void setup() {
+  /*
+  Serial.begin(500);
+  while (!Serial);
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("initialization failed!");
+  }
+  myFile = SD.open("test.bin", FILE_WRITE);
+  if (!myFile) // file had to be created
+    Serial.println("file had to be created");
+  Serial.println("attempting write to file");
+  // prints a shit load of Ls
+  // need a stopping criteria
+  while (1) {
+    myFile.write(76);
+  }
+
+  myFile.close();
+  */
+
+#if CAN_TEST == 0
+#else
+  // init CAN bus
+  CANReceiver.begin();
+  pinMode(led, OUTPUT);
+  Serial.println("CAN Receiver Initialized");
+#endif
+  pinMode(24, INPUT_PULLDOWN);
+  pinMode(25, INPUT_PULLDOWN);
+  //Serial.println(CANReceiver.defaultMask.id);
+  delay(2000);
+}
+
+void loop() {
+  Serial.println("Polling");
+#if CAN_TEST == 0
+#else
+  if(CANReceiver.available())
+    Serial.println("Found a frame");
+  /*
+  while(CANReceiver.read(msg)) {
+    break;
+    // toggle LEDs
+    digitalWrite(led, !digitalRead(led));
+    Serial.print("Receiving: ");
+    for(int i=0; i<msg.len; i++) {
+      Serial.print(msg.buf[i]); Serial.print(" ");
+    }
+    Serial.println("");
+  }*/
+  Serial.print(digitalRead(24)); //Serial.println(digitalRead(25));
+#endif
+  delay(100);
+}
---
+++ transmitter/transmitter.ino
@@ -0,0 +1,38 @@
+#include "FlexCAN.h"
+#include "kinetis_flexcan.h"
+// hello
+int led = 13;
+// create CAN object
+FlexCAN CANTransmitter(16000000/100000);
+static CAN_message_t msg;
+
+void setup() {
+  // init CAN bus
+  CANTransmitter.begin();
+  pinMode(led, OUTPUT);
+  delay(1000);
+  pinMode(24, INPUT_PULLDOWN);
+  pinMode(25, INPUT_PULLDOWN);
+  Serial.println("CAN Transmitter Initialized");
+  delay(2000);
+}
+
+void loop() {
+  //Serial.print("Sending: ");
+  msg.id = 0xFFF;
+  msg.len = 2;
+  msg.ext = 0;
+  for(int i=0; i<msg.len; i++) {
+    msg.buf[i] = 0;
+    //Serial.print(msg.buf[i]); Serial.print(" ");
+  }
+  Serial.println("");
+  // stops when no buffer can be found
+  if (CANTransmitter.write(msg) == 0) {
+    while(true);
+    return;
+  }
+  digitalWrite(led, !digitalRead(led));
+  //Serial.print(digitalRead(24)); //Serial.print(digitalRead(25));
+  delay(1000);
+}
